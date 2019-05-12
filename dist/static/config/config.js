640a63365817affd17a4439730be8ccf34df4645
Princessxia <987875642@qq.com>
2019-05-12 16:53:23 +0800

    替换包

--- dist/static/config/config.js
+++
@@ -1,8 +0,0 @@
-var Host = 'http://www.artronpano.com/'
-//var artronHttp = 'http://192.168.140.146:8080/artron-photography/'
-//var imgHttp = 'http://192.168.140.146:8088'
-
-var artronHttp = 'http://192.168.61.111:8080/artron-photography/'
-var imgHttp = 'http://192.168.61.111:8080'
-
-var addItional = '?format=json&ignore=false'
